fa52f3231f908bbb5a7cd3548646043214b7d09e
cashmonides <cashmonides@gmail.com>
2015-08-20 16:29:31 -0400

    added open_bracket_tagging

--- word_selector.js
+++ word_selector.js
@@ -51,26 +51,25 @@ var WordSelector = function(element_id, words){
                 var end = this.previous_id > id ? this.previous_id : id;
                 for (var i = start; i <= end; i++) {
                     var e = document.getElementById(i);
-                    this.words_in_play.add(i + "");
+                    this.words_in_play.add(i + "");         //todo we should make a consistent conventions
                     e.style.background = "red";
                 }
             }
         } else {
             var e = document.getElementById(id);
-            if (this.words_in_play.has(id)){
+            if (this.words_in_play.has(id)){                        //if...tests if it's already highlighted, in which case it gets unhighlighted
                 this.words_in_play.delete(id);
                 e.style.background = "white";
-            } 
-            
-            // else if (word === "(") {
-            //     var indices_of_target_clause = this.words_to_clauses[id].indices;
-            //     for (i  = 0; i < indices_of_target_clause.length; i++) {
-            //         var e2 = document.getElementById(indices_of_target_clause[i]);
-            //         this.words_in_play.add(indices_of_target_clause[i]);
-            //         e2.style.background = "red";
-            //     }
-            // } 
-            
+            } else if (word === '(' || word === '/') {              //todo this is the addition here
+                var indices_of_open_bracket_clause = this.open_bracket_clause(id);
+                //add these indices to words_in_play and turn red
+                for (var i = 0; i < indices_of_open_bracket_clause.length; i++) {
+                    this.words_in_play.add(indices_of_open_bracket_clause[i]);      //todo turn i into a sring?
+                    var e2 = document.getElementById(indices_of_open_bracket_clause[i]);
+                    e2.style.background = "red";
+                }
+            }
+            //this deals with the case of neither being highlighted nor a metacharacter
             else {
                 this.words_in_play.add(id);  // TODO - why is ID a string?
                 e.style.background = "red";
@@ -110,5 +109,66 @@ var WordSelector = function(element_id, words){
     
         
     };
-    
+
+
+
+
+    //todo new functionality below
+    //goal: to be able to click on an open bracket and wordselector only selects/highlights the words contained in the brackets
+
+    //argument will be an index (the index of the bracket user has clicked on)
+    //this will return a list of indices
+    this.open_bracket_clause = function (i) {
+        var list = [i];
+        //depth is a measure of how "deep" we are in the subordination, i.e. how many levels deep we are
+        var depth = 0;
+        //advance the index
+        i++;
+        //while true is a useful way to run a loop that only breaks when you want it to
+        while (true) {
+            //we might get to the end without hitting any break-inducing character - namely ')' or '/')
+            if (this.words[i] === undefined) {
+                break;
+            }
+            //so first we deal with the scenario of not finishing our current clause but rather entering another subordinate clause
+            else if (this.words[i] === '(') {
+                //another layer of subordination has been entered and thus we increase the depth by  one layer
+                depth++;
+            }
+            // or secondly we might close our current bracket
+            else if (this.words[i] === ')') {
+                //check if we are under any subordination
+                if (depth === 0) {
+                    //if so, we consider our clause to have ended
+                    //so first we need to push the close bracket
+                    list.push(i);
+                    //secondly we need to exit the loop because we're done
+                    break;
+                }
+                //but we might be under a subordination
+                else {
+                    // in which case we need to decrease the depth, because we've left a layer of subordination
+                    depth--;
+                }
+            }
+            //we might have reached coordination at the top level
+            else if (this.words[i] === '/') {
+                //in which case we wan't to push everything before and break
+                if (depth === 0) {
+                    break;
+                }
+            }
+            //or we might not hit any metacharacter, and in that case we just add indices to our list
+            else if (depth === 0) {
+                list.push(i);
+            }
+            i++;
+        }
+        return list;
+    };
+
+
+
+
+
 }
