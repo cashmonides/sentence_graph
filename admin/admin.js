13826a9398e93206273e7947201639f576b183b3
cashmonides <cashmonides@gmail.com>
2015-11-29 06:10:39 +0000

    organized animals into sequence

--- admin/admin.js
+++ admin/admin.js
@@ -27,10 +27,15 @@ function callback2(data) {
             children: [
                 {tag: "li", text: "name = " + users[key].profile.name},
                 {tag: "li", text: "max module = " + max_module(users[key])},
-                {tag: "li", text: report_accuracy(users[key])}
+                {tag: "li", text: report_accuracy(users[key])},
+                {tag: "li", text: "aggregate accuracy = " +  "PLACEHOLDER"}
             ]
         }, e)
     }
+}
+
+function get_aggregate_accuracy(user) {
+    
 }
 
 
@@ -41,7 +46,6 @@ function report_accuracy(user) {
         // console.log("DEBUG key = ", key);
         // console.log("DEBUG output = ", get_current_stats2(user, key));
         stats_map[key] = get_current_stats2(user,key);
-        stats_map["blank key"] = "blank value";
     }
     
     
@@ -80,8 +84,22 @@ function max_module(user) {
 }
 
 
-    
-    
+//todo make this a general function
+function get_max_accuracy (user, module_id, iteration) {
+   var order = get_module_order();
+   var UNIVERSAL_MODULE = ALL_MODULES[module_id];
+   var mod = user.history[module_id];
+   if (!mod) {return ''}
+   
+   var correct = mod.metrics[iteration][0];
+   
+   var total = Object.keys(mod.metrics[iteration]).
+        map(function (x) {return mod.metrics[iteration][x]}).
+        reduce(function (a, b) {return a + b});
+        
+    return Math.floor (correct/total);
+}
+
 function get_current_stats2 (user, module_id) {
     console.log("entering get_current_stats2");
     console.log("DEBUG module_id = ", module_id);
@@ -106,7 +124,7 @@ function get_current_stats2 (user, module_id) {
     
     
     
-    //todo rename something like get_max_accuracy
+    
     var get_accuracy = function (iteration) {
         return Math.floor(100 * mod.metrics[iteration][0]
         / Object.keys(mod.metrics[iteration]).
@@ -121,6 +139,8 @@ function get_current_stats2 (user, module_id) {
     var linebreak = document.createElement("br");
     var threshold = UNIVERSAL_MODULE.threshold;
     console.log("DEBUG threshold = ", threshold);
+    var aggregate_accuracy_list = [];
+    
     
     if (mod.in_progress == true && mod.iteration == 0) {
         // advance
@@ -134,8 +154,98 @@ function get_current_stats2 (user, module_id) {
     } else if (mod.in_progress == false && mod.iteration > 0) {
         // completed, not in progress
         //return highest accuracy
-        return Math.max.apply(null,
-        Object.keys(mod.metrics).map(get_accuracy)) + "%";
+        var accuracy = Math.max.apply(null,
+        Object.keys(mod.metrics).map(get_accuracy));
+        
+        aggregate_accuracy_list.push(accuracy);
+        
+        
+         function add(a, b) {
+            return a + b;
+        }
+        
+        var aggregate_accuracy = aggregate_accuracy_list.reduce(add, 0);
+
+       
+        
+        
+        
+        var output = "accuracy = " + accuracy + "%" + "aggregare_accuracy_list" + aggregate_accuracy_list + "aggregate_accuracy = " + aggregate_accuracy + "%" ;
+        return JSON.stringify(output);
+    } else if (mod.in_progress == false && mod.iteration == 0) {
+        return ""
+    } else {
+        throw new Error('No case successfully caught.')
+    }
+    // else if (mod.in_progress == "false" && mod.iteration == 0) {
+    //     return ""
+    // }
+}
+
+
+function get_aggregate_accuray2 (user) {
+    
+    for (key in user.history) {
+    
+        var get_accuracy = function (iteration) {
+            return Math.floor(100 * mod.metrics[iteration][0]
+            / Object.keys(mod.metrics[iteration]).
+            map(function (x) {return mod.metrics[iteration][x]}).
+            reduce(function (a, b) {return a + b}))
+        }
+    
+   aggregate_accuracy_list.push(accuracy);
+        
+    }
+    
+    function add(a, b) {
+        return a + b;
+    }
+    
+    var aggregate_accuracy = aggregate_accuracy_list.reduce(add, 0);
+    
+    return aggregate_accuracy;
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+    var linebreak = document.createElement("br");
+    var threshold = UNIVERSAL_MODULE.threshold;
+    console.log("DEBUG threshold = ", threshold);
+    var aggregate_accuracy_list = [];
+    
+    
+    if (mod.in_progress == true && mod.iteration == 0) {
+        // advance
+        return mod.progress + '/' + threshold;// + UNIVERSAL_MODULE.threshold
+    } else if (mod.in_progress == true && mod.iteration > 0) {
+        // improve
+        return "module: " + mod.id + "status = improving";
+        // return 'advancing ' + get_accuracy(mod.iteration) + '% previous best ' +
+        // Math.max.apply(null, Object.keys(mod.metrics).filter(
+        // function (x) {return x != mod.iteration}).map(get_percentage))
+    } else if (mod.in_progress == false && mod.iteration > 0) {
+        // completed, not in progress
+        //return highest accuracy
+        var accuracy = Math.max.apply(null,
+        Object.keys(mod.metrics).map(get_accuracy));
+        
+        
+
+       
+        
+        
+        
+        var output = "accuracy = " + accuracy + "%" + "aggregare_accuracy_list" + aggregate_accuracy_list + "aggregate_accuracy = " + aggregate_accuracy + "%" ;
+        return JSON.stringify(output);
     } else if (mod.in_progress == false && mod.iteration == 0) {
         return ""
     } else {
