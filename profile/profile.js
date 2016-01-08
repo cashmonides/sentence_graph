527dfaeb98bd295acd3091f80db492b1f82b3b1d
cashmonides <cashmonides@gmail.com>
2016-01-08 02:37:43 +0000

    cleaned up time logs

--- profile/profile.js
+++ profile/profile.js
@@ -70,6 +70,29 @@ ProfilePage.display_profile = function() {
     this.build_progress_table(this.user);
 };
 
+//dummy function for testing
+//proof of concept - how to persist.get something simple like user name
+ProfilePage.alert_name = function () {
+    Persist.get([path], display_name);
+}
+
+function display_name(data) {
+    console.log("name = ", data);
+}
+
+
+// //path terminates in a node with a time stamp
+// ProfilePage.get_time_metrics = function () {
+//     Persist.get([path], update_time_metrics);
+// }
+
+// //
+// function update_time_metrics(data) {
+//     var new_start_time = Date.now();
+//     var new_list = data.push(new_start_time);
+//     //persist statement, setting 
+// }
+
 
 
 // return: int which is number of levels from current_module
